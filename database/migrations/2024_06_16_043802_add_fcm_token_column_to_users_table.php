92119f398e4380c814628d0c17111385101d39cb
rikzanx <erik.kasbam@gmail.com>
2024-06-16 15:45:26 +0700

    add nullable

--- database/migrations/2024_06_16_043802_add_fcm_token_column_to_users_table.php
+++ database/migrations/2024_06_16_043802_add_fcm_token_column_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('fcm_token');
+            $table->string('fcm_token')->nullable();
         });
     }
 
